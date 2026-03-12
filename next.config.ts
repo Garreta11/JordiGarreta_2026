b960ef6ca6ff931c12fb18c8cf120592f08992ab
garreta11 <jordigarreta11@gmail.com>
2026-03-12 19:06:01 +0100

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,22 @@ import type { NextConfig } from "next";
 import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  turbopack: {}, // 👈 empty object disables custom turbopack handling
+  turbopack: {
+    rules: {
+      '*.glsl': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.vert': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.frag': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+    },
+  },
   webpack(config: Configuration) {
     config.module?.rules?.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
