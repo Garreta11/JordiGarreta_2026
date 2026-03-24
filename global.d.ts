38f1b3894954f815cec50ea05f9e9a0a785ad4fe
garreta11 <jordigarreta11@gmail.com>
2026-03-24 09:49:31 +0100

    improve errors globals glsl

--- global.d.ts
+++ global.d.ts
@@ -5,7 +5,3 @@ declare global {
     exitHomeSketch?: (callback: () => void) => void;
   }
 }
-
-declare module '*.glsl' { const value: string; export default value; }
-declare module '*.vert' { const value: string; export default value; }
-declare module '*.frag' { const value: string; export default value; }
